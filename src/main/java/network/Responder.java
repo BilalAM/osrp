7f166137556c5b97bb4356c0952db3e2a9024e78
BilalAM <bilal.asif.97@gmail.com>
2017-12-27 00:38:17 +0500

    responder in progress !

--- src/main/java/network/Responder.java
+++ src/main/java/network/Responder.java
@@ -1,5 +1,36 @@
 package network;
 
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+import network.Router;
+import network.Table;
+
 public class Responder {
+
+	private Router selfRouter;
+
+	public Responder(Router selfRouter) {
+		this.selfRouter = selfRouter;
+	}
+
+	// Responder responder = new Responder() ; responder.getTables();
+	// responder.checkAndUpdate();
+
+	/**
+	 * 
+	 * @param selfServer
+	 * @return list of tables of the directly connected routers from "selfRouter"
+	 */
+	public List<Table> getTables(ServerSocket selfServer) {
+		List<Table> tables = new ArrayList<>();
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tables;
+	}
 
 }
