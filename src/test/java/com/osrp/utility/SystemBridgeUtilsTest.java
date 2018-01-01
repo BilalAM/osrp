c0e4c9737b395e5a5538239c5045897cb75aebf1
MSaifAsif <saifasifmirza@gmail.com>
2018-01-01 19:46:39 +0100

    added sigar api

---
+++ src/test/java/com/osrp/utility/SystemBridgeUtilsTest.java
@@ -0,0 +1,18 @@
+package com.osrp.utility;
+
+import org.hyperic.jni.ArchNotSupportedException;
+import org.hyperic.sigar.SigarException;
+import org.junit.jupiter.api.Disabled;
+
+/**
+ * @author saifasif
+ */
+public class SystemBridgeUtilsTest {
+
+    @Disabled
+    public void read() throws ArchNotSupportedException, SigarException {
+        SystemBridgeUtils.read();
+    }
+
+
+}
