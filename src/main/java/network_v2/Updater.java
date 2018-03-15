dead62a54de560fc8525a7aa3fa99a8e73455472
BilalAM <bilal.asif.97@gmail.com>
2018-03-15 22:40:37 +0500

    updater in progress...

--- src/main/java/network_v2/Updater.java
+++ src/main/java/network_v2/Updater.java
@@ -1,25 +1,45 @@
 package network_v2;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Updater {
 
-    public void Update(Table toUpdate, Table forUpdate) throws Exception {
+
+
+
+    public void Update(Table toUpdate, Table forUpdate) {
         List<Table.Entry> tempEntries = new ArrayList<>();
+        try {
+            for (Table.Entry forEntry : forUpdate.getEntries()) {
 
-        for (Table.Entry forEntry : forUpdate.getEntries()) {
-            for (Table.Entry toEntry : toUpdate.getEntries()) {
-                if (forEntry.next.equals(InetAddress.getByName("0.0.0.0")) && toEntry.next.equals(InetAddress.getByName("0.0.0.0"))) {
-                    continue;
-                }
-                if (!(forEntry.destination.equals(toEntry.destination))) {
+                inner: for (Table.Entry toEntry : toUpdate.getEntries()) {
+                    if(isLoopEntry(toEntry,forEntry)){
+                        System.out.println("**WE HAVE A LOOP ENTRY !! **");
+                        //skip this entry...
+                        continue inner;
+                    }
+                    if (!(forEntry.destination.equals(toEntry.destination))) {
 
-                    tempEntries.add(new Table.Entry(toUpdate.source, forEntry.destination, forUpdate.source, forEntry.cost + 1));
+                        tempEntries.add(new Table.Entry(toUpdate.source, forEntry.destination, forUpdate.source, forEntry.cost + 1));
+                    }
                 }
             }
+            toUpdate.getEntries().addAll(tempEntries);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        toUpdate.getEntries().addAll(tempEntries);
     }
+
+    private static boolean isLoopEntry(Table.Entry e1 , Table.Entry e2) throws UnknownHostException{
+        if(e1.source.equals(e2.destination)){
+            if(e1.destination.equals((e2.source))){
+                return e1.next.equals(InetAddress.getByName("0.0.0.0")) && e2.next.equals(InetAddress.getByName("0.0.0.0"));
+            }
+        }
+        return false;
+    }
+
 }
