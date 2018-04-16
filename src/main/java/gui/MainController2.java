de93376a49a654e321f124f462a1223e3bbf065e
BilalAM <bilal.asif.97@gmail.com>
2018-04-16 12:44:13 +0500

    packet forwarding and other changes

--- src/main/java/gui/MainController2.java
+++ src/main/java/gui/MainController2.java
@@ -13,7 +13,6 @@
     import network_v2.Packet;
     import network_v2.Host;
 
-    import java.lang.management.PlatformManagedObject;
     import java.net.InetAddress;
 
     public class MainController2 {
@@ -58,33 +57,37 @@
 
         @FXML
         private Button SEND_PCKT = new Button();
+
         /**
          * A host is connecting to a router , default ip is 192.168.15.120
          */
         @FXML
         private void connectToRouter() {
             // selfHost.connectToRouter(IPsHelper.getPrivateIp("wlo1"));
-            Packet packet = new Packet();
-            try {
-                packet.setSourceAddress(InetAddress.getByName(GUIUtils.getPrivateIp("wlxa0f3c12c7d2a")));
-                packet.setPacketMessage("HELLO THIS IS A PACKET MESSAGE :)");
-                packet.setDestinationAddress(InetAddress.getByName(DEST_IP.getText()));
-                self.requestHostConnection(packet);
-            } catch (Exception e) {
-                e.printStackTrace();
+
+            // magical packet-nizer loop to create and send packets
+            for (int i = 0; i < 5000; i++) {
+                Packet packet = new Packet();
+                try {
+                    packet.setSourceAddress(InetAddress.getByName(GUIUtils.getPrivateIp("wlo1")));
+                    packet.setPacketMessage("HELLO THIS IS A PACKET MESSAGE :)");
+                    packet.setDestinationAddress(InetAddress.getByName(DEST_IP.getText()));
+                    self.requestHostConnection(packet);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                hostCmd.setText(selfHost.getCmd());
+                System.out.println("host is connected to router");
+                onHost();
             }
-            hostCmd.setText(selfHost.getCmd());
-            System.out.println("host is connected to router");
         }
-
-
 
 
         /**
          * connecting to a seperate router based on ip
          */
         @FXML
-        private void requestConnection(){
+        private void requestConnection() {
             self.requestConnection(REQUEST_IP.getText());
             cmd.setText(self.getCMD());
         }
@@ -94,8 +97,9 @@
          * turns on the router , router is listening to other router's request connections too
          */
         @FXML
-        private void on(){
+        private void on() {
             System.out.println("ROUTER IS ON....WAITING FOR CONNECTIONS....");
+            ON_HOST.disableProperty().setValue(true);
             startAcceptTask();
         }
 
@@ -104,7 +108,7 @@
          * router is listening for other HOSTS connections too
          */
         @FXML
-        private void onHost(){
+        private void onHost() {
             System.out.println("ROUTER IS WAITING FOR HOST ALSO....");
             startHostAcceptTask();
         }
@@ -124,17 +128,27 @@
          * router is constantly listening for other routers that are sending their tables to it
          */
         @FXML
-        public void receiveTable(){
+        public void receiveTable() {
             UPDATE_BUTTON.setDisable(true);
             startTablesTask();
         }
 
+        @FXML
+        public void onListenPacket(){
+            Thread backGround = new Thread(){
+                @Override
+                public void run() {
+                    runAcceptTask2();
+                }
+            };
+            backGround.setDaemon(true);
+            backGround.start();
+        }
 
         // MULTITHREADED OPERATIONS FUNCTIONALITIES
 
 
-
-        private void startHostAcceptTask(){
+        private void startHostAcceptTask() {
             Runnable task = () -> runHostAcceptTask();
 
             Thread backGround = new Thread(task);
@@ -144,16 +158,20 @@
 
 
         private void startAcceptTask() {
-
-            Runnable task = () -> runAcceptTask();
-
             // Run the task in a background thread
-            Thread backgroundThread = new Thread(task);
+            Thread backgroundThread = new Thread() {
+                @Override
+                public void run() {
+                    runAcceptTask();
+                }
+            };
             // Terminate the running thread if the main.java.gui exits
             backgroundThread.setDaemon(true);
-            // Start the thread
             backgroundThread.start();
+
+
         }
+
         private void startTablesTask() {
 
             Runnable task = () -> runTablesTask();
@@ -167,30 +185,51 @@
         }
 
         private void runHostAcceptTask() {
+            //while (true) {
+            self.acceptHostConnection();
+            Platform.runLater(() -> {
+                cmd.setText("a host is here ! :3 ");
+            });
+            //}
+        }
+
+
+
+        private void runAcceptTask2(){
+           while (true){
+               try {
+                   //self.recieveAndforwardPacket();
+                   // Thread.sleep(100);
+               }catch (Exception e){
+                   e.printStackTrace();
+               }
+               self.receiveAndForwardThePacket();
+                Platform.runLater(() -> {
+                    cmd.setText("hello love :)");
+                });
+            }
+        }
+        private void runAcceptTask() {
             while (true) {
-                self.acceptHostConnection();
-                Platform.runLater(() -> {
-                    cmd.setText("a host is here ! :3 ");
-                });
+                try {
+                    //Thread.sleep(10);
+
+                    self.acceptConnections();
+                    Platform.runLater(() -> {
+                        cmd.setText(self.getCMD());
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
 
 
-        private void runAcceptTask(){
-            while (true){
-                self.acceptConnections();
-                Platform.runLater(() -> {
-                    cmd.setText(self.getCMD());
-                });
-            }
-        }
-
-
-        private void runTablesTask(){
-            while(true){
+        private void runTablesTask() {
+            while (true) {
                 try {
                     Thread.sleep(1000);
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 responder.recieveTables(self);
@@ -199,7 +238,5 @@
                 });
             }
         }
-
-
     }
 
