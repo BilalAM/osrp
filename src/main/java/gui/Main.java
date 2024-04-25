5f4f8c9a4ba4db35a4c1e71126d1cd363897080e
BilalAM <bilal.asif.97@gmail.com>
2024-04-25 22:54:24 +0200

    IT IS ALIVE.

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -4,13 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
+import java.io.PrintStream;
 
 public class Main extends Application {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -26,12 +24,13 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/sample2.fxml"));
+            loader.setLocation(getClass().getResource("/SideBarFXML.fxml"));
             Parent root = loader.load();
             primaryStage.setTitle("OSRP By Bilal , Neha and Umer");
-            primaryStage.setScene(new Scene(root, 1150, 695));
+            primaryStage.setScene(new Scene(root, 1166, 568));
             primaryStage.show();
-
+            PrintStream stream = new PrintStream("logging.txt");
+          //  System.setOut(stream);
         } catch (Exception e) {
             e.printStackTrace();
         }
